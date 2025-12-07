11c6a31c38e57757d0a736d067e50380fddc7f50
Roshankumar350 <roshankumar350@gmail.com>
2025-12-07 20:40:30 +0530

    async support for local host

--- APIContractCompatability/ResourceManager/ResourceManager.swift
+++ APIContractCompatability/ResourceManager/ResourceManager.swift
@@ -10,6 +10,13 @@ import Foundation
 protocol ResourceManager {
     associatedtype T: Decodable
     func fetchUsers(for apiContract: APIContractVersion) throws -> [T]
+    func fetchAsyncUsers(for apiContract: APIContractVersion) async throws -> [T]
+}
+
+extension ResourceManager {
+    func fetchAsyncUsers(for apiContract: APIContractVersion) async throws -> [T] {
+        throw NSError(domain: "Not implemented", code: 0, userInfo: nil)
+    }
 }
 
 enum APIContractVersion: String {
--- APIContractCompatability/UserViewModel.swift
+++ APIContractCompatability/UserViewModel.swift
@@ -9,8 +9,10 @@ import Foundation
 import Combine
 
 final class UserViewModel<Manager: ResourceManager>: ObservableObject where Manager.T == User {
-    @Published var users: [User] = []
-    let resourceManager: Manager
+    @Published private(set) var users: [User] = []
+    private let resourceManager: Manager
+    /// Toggle this to fetch from local host: isAsyncFetchEnabled = true
+    let isAsyncFetchEnabled: Bool = false
     
     init(resourceManager: Manager) {
         self.resourceManager = resourceManager
@@ -23,6 +25,18 @@ final class UserViewModel<Manager: ResourceManager>: ObservableObject where Mana
             // Notice that consumer side has no impact on change of API contract.
             // Try setting `APIContractVersion` to v1 and v2
             users = try resourceManager.fetchUsers(for: apiContract)
+            print("ApiContract version is \(apiContract) and users are: \(users)")
+        } catch {
+            users = []
+        }
+    }
+    
+    func fetchAsyncUsers(for apiContract: APIContractVersion = .v2) async  {
+        do {
+            // Version of API can be set from here,
+            // Notice that consumer side has no impact on change of API contract.
+            // Try setting `APIContractVersion` to v1 and v2
+            users = try await resourceManager.fetchAsyncUsers(for: apiContract)
             print("ApiContract version is \(apiContract) and users are: \(users)")
         } catch {
             users = []
